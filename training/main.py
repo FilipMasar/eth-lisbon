3a2cd8735f44f1617ae0442f01157d04dff6bdbb
Filip Masar <filip.masar99@gmail.com>
2023-05-13 22:28:59 +0100

    aggregation working

--- training/main.py
+++ training/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import glob, sys
+import glob, sys, subprocess, os
 
 import argparse
 
@@ -21,7 +21,33 @@ def local_training(input_dir, output_dir):
 
 
 def aggregation(input_dir, output_dir):
-    print("TODO")
+    # loop throug directories in input_dir
+    # for each directory, read model.txt
+    # compute average
+    # write to output_dir/model.txt
+
+    models = []
+
+    for dir in os.listdir(input_dir):
+        model_path = os.path.join(input_dir, dir, "outputs", "model.txt")
+        if os.path.isfile(model_path):
+            subprocess.run(["cat", model_path])
+            models.append(model_path)
+
+    print("Models:", models)
+    # loop through files and compute overall average
+    avg = 0
+    len = 0
+    for model in models:
+        with open(model, 'r') as f:
+            data = f.readlines()
+            avg += float(data[0]) * int(data[1])
+            len += int(data[1])
+
+    print("Overall average:", avg / len)
+
+    with open(f'{output_dir}/model.txt', 'w') as f:
+        f.write(str(avg / len) + '\n')
 
 
 if __name__ == "__main__":
