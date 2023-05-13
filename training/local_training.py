7fbcfeca05d04eff1eae1f6e20fce954974bad33
Filip Masar <filip.masar99@gmail.com>
2023-05-13 17:29:06 +0100

    add some logs

--- training/local_training.py
+++ training/local_training.py
@@ -1,6 +1,9 @@
-import csv
+import csv, subprocess
 
 dataset = '/part1.csv'
+
+# list files in current directory
+subprocess.run(["ls", "-al"])
 
 # Open the CSV file
 with open(dataset, newline='') as csvfile:
@@ -18,6 +21,9 @@ with open(dataset, newline='') as csvfile:
 # compute average of last column
 last_column = [float(row[-1]) for row in data]
 average = sum(last_column) / len(data)
+
+print("Average: " + str(average))
+print("Number of rows: " + str(len(data)))
 
 # write to file average and number of rows
 with open('model.txt', 'w') as f:
